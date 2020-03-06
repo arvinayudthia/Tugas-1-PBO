f19075c61702b2e1f9efcb9406fd508b95dbc029
arvinayudthia <61867331+arvinayudthia@users.noreply.github.com>
2020-03-06 21:45:38 +0800

    Add files via upload

---
+++ Number2.java
@@ -0,0 +1,5 @@
+class Number2{
+public static void main(String[] args){
+System.out.println("Belajar Bahasa Pemrograman Java");
+}
+}
---
+++ Number3.java
@@ -0,0 +1,5 @@
+class Number3{
+public static void main(String[] args){
+System.out.println("Selasai Belajar Pemrograman Java");
+}
+}
